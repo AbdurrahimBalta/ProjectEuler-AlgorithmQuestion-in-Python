2d1d2dadd4391c02bc14bda3d4ea463609d74fbc
LENOVO <apro.yazilim59@gmail.com>
2019-08-04 08:50:38 +0300

    100001. asal sayıyı bulan algoritma

--- Question7.py
+++ Question7.py
@@ -2,7 +2,7 @@ from math import sqrt
 """By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 What is the 10 001st prime number?"""
 # İlk altı asal sayıyı listeleyerek: 2, 3, 5, 7, 11 ve 13, 6. asalın 13 olduğunu görebiliriz.
-# 10001. asal sayı nedir ?
+# 10001. asal sayı nedir ??
 
 def drange(basla, dur, atla):
     r = basla
