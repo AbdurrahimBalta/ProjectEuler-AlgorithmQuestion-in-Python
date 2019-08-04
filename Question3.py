d44e5c09c631b24fb9af96aaba592e285176d9a2
LENOVO <apro.yazilim59@gmail.com>
2019-08-04 08:29:09 +0300

    En büyük asal böleni bulan algoritma (Başka sorular için method haline geldi)

--- Question3.py
+++ Question3.py
@@ -1,17 +1,14 @@
 #he prime factors of 13195 are 5, 7, 13 and 29.
-#What is the largest prime factor of the number 600851475143 ?
-#13195 sayısının asal bölenleri 5-7-13-29 dur buna göre aşşağıdaki sayının en büyük asal böleni nedir
+#What is t#13195 sayısının asal bölenleri 5-7-13-29 dur buna göre aşşağıdaki sayının en büyük asal böleni nedir
 
-Sayi : int = 600851475143
-AsalBolen : int = 2
-Dizi = list()
-#Sayinin tüm asal bölenleri bulunur
-while Sayi >= AsalBolen:
-    if Sayi % AsalBolen == 0:
-        Sayi = Sayi / AsalBolen
-        Dizi.insert(len(Dizi), AsalBolen)
-    else:
-        AsalBolen += 1
-print(Dizi)
-print(Dizi[len(Dizi)-1])
-
+def asal(Sayi):
+    AsalBolen = 2
+    Dizi = list()
+    while Sayi >= AsalBolen:
+        if Sayi % AsalBolen == 0:
+            Sayi = Sayi / AsalBolen
+            Dizi.insert(len(Dizi),AsalBolen)
+        else:
+            AsalBolen += 1
+    return Dizi
+print(asal(600851475143))
